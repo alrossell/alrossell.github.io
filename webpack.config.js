12bb81726a77205c4dc8070a2e1ea009d9e36771
Andrew Rossell <andrewrossell@pop-os.localdomain>
2022-07-21 12:29:50 -0700

    Configure React app for deployment to GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -30,30 +30,3 @@ module.exports = {
 	plugins: [htmlPlugin]
 };
 
-// var path = require('path');
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-// 	mode: 'development',
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'index_bundle.js',
-//     publicPath: '/'
-//   },
-//   module: {
-	
-//     rules: [
-//       { test: /\.(js)$/, use: 'babel-loader' },
-//       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-//     ]
-//   },
-//   devServer: {
-//     historyApiFallback: true,
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'src/index.html'
-//     })
-//   ]
-// };
